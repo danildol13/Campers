98a4bd38fd083e68f26698375c2e86c2aaf09097
Danylo <161326427+Daniel2-1@users.noreply.github.com>
2026-03-24 16:31:11 +0100

    new

--- Components/BookingForm/BookingForm.tsx
+++ Components/BookingForm/BookingForm.tsx
@@ -1,7 +1,7 @@
 "use client";
 import Button from "../shared/Button/Button";
 import toast from "react-hot-toast";
-import css from './BookingForm.module.css'
+import css from "./BookingForm.module.css";
 
 export default function BookingForm() {
   const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
@@ -9,12 +9,16 @@ export default function BookingForm() {
     toast.success("Booking confirmed!");
   };
   return (
-    <form onSubmit={handleSubmit} className={css.formSection}>
-      <input type="text" name="name" />
-      <input type="email" name="email" />
-      <input type="text" name="bookingDate" />
-      <textarea name="comment"></textarea>
-      <Button type="submit">Send</Button>
-    </form>
+    <div>
+      <h2>Book your campervan now</h2>
+      <p>Stay connected! We are always ready to help you.</p>
+      <form onSubmit={handleSubmit} className={css.formSection}>
+        <input type="text" name="name" placeholder="Name*" />
+        <input type="email" name="email" placeholder="Email*" />
+        <input type="date" name="bookingDate" placeholder="" />
+        <textarea placeholder="Comment" name="comment"></textarea>
+        <Button type="submit">Send</Button>
+      </form>
+    </div>
   );
 }
--- Components/CamperDetails/Features/Features.tsx
+++ Components/CamperDetails/Features/Features.tsx
@@ -1,4 +1,5 @@
 import { Infos, firstCategories, vehicleDetails } from "@/types/camper";
+import css from "./Features.module.css";
 
 interface Props {
   camper: Infos;
@@ -6,18 +7,36 @@ interface Props {
 
 export default function Features({ camper }: Props) {
   return (
-    <div>
-      {firstCategories.map(
-        (key) => camper[key] && <span key={key}>{key}</span>,
-      )}
-      {camper.transmission === "automatic" && <span>automatic</span>}
-      {camper.engine && <span>{camper.engine}</span>}
-      {vehicleDetails.map((key) => (
-        <div key={key}>
-          <span>{key}</span>
-          <span>{camper[key]}</span>
+    <div className={css.featuresCard}>
+      <div className={css.categoriesCard}>
+        {camper.transmission === "automatic" && (
+          <div className={css.categories}>
+            <svg width={20} height={20}>
+              <use href="/allicons.svg#automatic"></use>
+            </svg>
+            <span className={css.categoryName}>automatic</span>
+          </div>
+        )}
+        {firstCategories.map((category) => (
+          <div key={category} className={css.categories}>
+            <svg width={20} height={20}>
+              <use href={`/allicons.svg#${category}`}></use>
+            </svg>
+            <span className={css.categoryName}>{category}</span>
+          </div>
+        ))}
+      </div>
+      <div className={css.detailsBlock}>
+        <p className={css.detailsTitle}>Vehicle details</p>
+        <div className={css.details}>
+          {vehicleDetails.map((key) => (
+            <div className={css.oneDetail} key={key}>
+              <span>{`${key[0].toUpperCase()}${key.slice(1)}`}</span>
+              <span>{`${camper[key][0].toUpperCase()}${camper[key].slice(1)}`}</span>
+            </div>
+          ))}
         </div>
-      ))}
+      </div>
     </div>
   );
 }
